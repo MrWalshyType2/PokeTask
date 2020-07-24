fb431aa43bb1eb2c63ee48e36359334067fded17
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-07-24 13:34:56 +0100

    Reformatted docs, removed -userLowerLimit from fetch

--- Abilities.js
+++ Abilities.js
@@ -1,38 +1,42 @@
 let userUpperLimit;
 let userLowerLimit;
 
-function sendValues(){
-    userLowerLimit = document.getElementById("userLowerLimit").value;
-    userUpperLimit = document.getElementById("userUpperLimit").value;
-    limit(userLowerLimit, userUpperLimit);
-    console.log("yo we getting intothis funtcion?");
+function sendValues() {
+  userLowerLimit = document.getElementById("userLowerLimit").value;
+  userUpperLimit = document.getElementById("userUpperLimit").value;
+  limit(userLowerLimit, userUpperLimit);
+  console.log("yo we getting intothis funtcion?");
 }
 
 function limit(userLowerLimit, userUpperLimit) {
-    fetch('https://pokeapi.co/api/v2/ability/?offset=' + userLowerLimit + '&limit=' + (userUpperLimit-userLowerLimit))
-    .then(
-    function(response) {
+  fetch(
+    "https://pokeapi.co/api/v2/ability/?offset=" +
+      userLowerLimit +
+      "&limit=" +
+      (userUpperLimit)
+  ).then(function (response) {
     if (response.status !== 200) {
-    console.log('Looks like there was a problem. Status Code: ' +
-    response.status);
-    return;
+      console.log(
+        "Looks like there was a problem. Status Code: " + response.status
+      );
+      return;
     }
-    response.json().then(function(data) {
-        console.log(data);
-        for (let i = 0; i < 293; i++) {
+    response.json().then(function (data) {
+      console.log(data);
+      for (let i = 0; i < 293; i++) {
         console.log(data.results[i].name);
         console.log(data.results[i].url);
-        
+
         let para = document.createElement("P"); // Create a <p> element
         para;
         para.className = "alert alert-danger col-md-8";
-        para.innerText = `The name is : ${(data.results[i].name)} \n
+        para.innerText = `The name is : ${data.results[i].name} \n
         The url is : ${data.results[i].url}`; // Insert text
 
         let myDiv = document.getElementById("myDiv");
-        
+
         myDiv.appendChild(para);
-        }
-        });
-        })
+      }
+    });
+  });
 }
